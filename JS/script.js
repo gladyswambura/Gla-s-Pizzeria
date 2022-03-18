deea8a894b155ec6dc2e8a7682b3bb323b153cd3
gladyswambura <gladyswahito7@gmail.com>
2022-03-18 13:00:15 +0300

    creates a navigation bar to our website

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+// $("#lightswitch").click(function () {
+//     var isDark = $("body").is(".dark");
+//     $("body").toggleClass("dark", !isDark);
+//     $(this).toggleClass("fa-moon", isDark).toggleClass("fa-sun", !isDark);
+// });
+
