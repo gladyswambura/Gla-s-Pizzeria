a8d1aa4bb99ff0a18ca0c1b73fa837213985d03c
gladyswambura <gladyswahito7@gmail.com>
2022-03-21 00:48:19 +0300

    fixes the NaN bug in totalAmount

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,13 @@
 
 var deliveryFee = 200;
 
-function pizza(pizzaFlavor, crustType, pizzaSize, toppings, totalAmount) {
-    this.pizzaFlavor = pizzaFlavor;
-    this.crustName = crustType;
-    this.sizeName = pizzaSize;
-    this.toppingName = toppings;
-    this.totalAmount = totalAmount
-}
+// function pizza(pizzaFlavor, crustType, pizzaSize, toppings, totalAmount) {
+//     this.pizzaFlavor = pizzaFlavor;
+//     this.crustName = crustType;
+//     this.sizeName = pizzaSize;
+//     this.toppingName = toppings;
+//     this.totalAmount = totalAmount
+// }
 
 $(document).ready(function () {
     $("button#submit").click(function (event) {
@@ -56,14 +56,24 @@ $("#Checkout").click(function () {
 $("#person-name").val("");
 $("#OptionDelivery").val("");
 
+
+// deliver
 $("#deliver").click(function (event) {
      event.preventDefault();
  
 var fullName = $('#person-name').val();
-var telephoneNumber = $('#phone-number').val();
-tel = telephoneNumber
-NameOfCustomer = fullName;
-var deliveryOption = $("#OptionDelivery option:selected").val();
+var deliveryOption = $('#optionDelivery option:selected').val();     
+var pizzaFlavor = $('#flavor option:selected').val();
+var pizzaSize = $('#pizza-size option:selected').val();
+var crustType = $('#crust option:selected').val();
+var toppings = $('#toppings option:selected').val();
+var additionalToppings = $('#extra-toppings option:selected').val();
+var totalNumberOfPizza = $('#pizza-number').val();
+
+var totalAmount = parseInt(totalNumberOfPizza) * (parseInt(pizzaSize) + parseInt(toppings) +
+    parseInt(additionalToppings) + parseInt(crustType));
+
+
 if (deliveryOption === "deliver") {
     $(".deliveryLocation").show();
 } else {
@@ -75,18 +85,19 @@ if (deliveryOption === "deliver") {
 });
 
 $("#finiish").click(function (event) {
+    
+    var fullName = $('#person-name').val();
+    var deliveryOption = $('#optionDelivery option:selected').val();
     var pizzaFlavor = $('#flavor option:selected').val();
     var pizzaSize = $('#pizza-size option:selected').val();
     var crustType = $('#crust option:selected').val();
     var toppings = $('#toppings option:selected').val();
     var additionalToppings = $('#extra-toppings option:selected').val();
-    var fullName = $('#person-name').val();
+    var totalNumberOfPizza = $('#pizza-number').val();
+
     var totalAmount = parseInt(totalNumberOfPizza) * (parseInt(pizzaSize) + parseInt(toppings) +
         parseInt(additionalToppings) + parseInt(crustType));
-    var totalNumberOfPizza = $('#pizza-number').val();
-    var telephoneNumber = $('#phone-number').val();
-    tel = telephoneNumber
-    NameOfCustomer = fullName;
+
     
    alert("Dear " + fullName + " "  + "Your" + " " + pizzaFlavor + " " + "that" + " " + "costs ksh." + " " + totalAmount + " " + 
     "will be delivered within thirty minutes." + "thank you for shopping with us" + "  "  + "regards (gla's Pizeria)" );
