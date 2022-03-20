9dbaa79030487dea24d5202c2bf42a334f4106f3
gladyswambura <gladyswahito7@gmail.com>
2022-03-20 08:37:25 +0300

    improves the layout of the form in the website

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,89 @@
-var total = 0;
+var total = [];
 var deliveryFee = 200;
-var crustPrice = new Array(250, 350, 300);
-var sizePrice = new Array(500, 800, 1100);
-var toppingsPrice = new Array(50, 300, 200, 150, 400);
+// var crustPrice = new Array(250, 350, 300);
+// var toppingsPrice = new Array(50, 300, 200, 150, 400);
 
-function TotalPrices(flavorName, crustName, sizeName, toppingName) {
+function totalPrices(flavorName, crustName, sizeName, toppingName, pizzaNumber) {
     this.flavorName = flavorName;
     this.crustName = crustName;
     this.sizeName = sizeName;
     this.toppingName = toppingName;
+    this, pizzaNumber = pizzaNumber
 }
 
 $(document).ready(function () {
     $("button#submit").click(function (event) {
-        $("button#submit").text("Add Pizza");
+        $("button#submit").alert("its you again");
+         event.preventDefault();
+});
 
-        // flavour
-     event.preventDefault();
-     var selectedFlavor = document.getElementsByName("flavors");
-     for (i = 0; i < selectedFlavor.length; i++) {
-         if (selectedFlavor[i].checked) {
-             var flavorName = selectedFlavor[i].value;
-         }
+      var pizzaPrice = new Array(500, 800, 1100);
+      Pizza.prototype.totalPrice = function () {
+          return (this.crustName + this.toppingName + this.sizeName) * this.pizzaNumber
+      };
+
+        //  size
+      Pizza.prototype.pizzaS = function () {
+          if (this.sizeName == 1100) {
+              return "Large"
+
+          } else if (this.sizeName == 800) {
+              return "Medium"
+
+          } else if (this.sizeName == 500) {
+              return "Small"
+          }
+      }
+
+    //   crust
+    Pizza.prototype.pizzaC = function () {
+        if (this.crustName == 250) {
+            return "Thin"
+
+        } else if (this.crustName == 350) {
+            return "Thick"
+
+        } else if (this.crustName == 300) {
+            return "Stuffed"
+        }
+    }
+
+    // topping
+    Pizza.prototype.pizzaT = function () {
+        if (this.toppingName == 150) {
+            return "Potato"
+        } else if (this.toppingName == 170) {
+            return "Mushrooms"
+        } else if (this.toppingName == 200) {
+            return "Black olives"
+        }
+    }
+
+    $("#checkout").click(function (event) {
+
+                var flavorName = $("#Flavour").val();
+
+                var sizeName = parseInt($("#Size").val());
+
+                var crustName = parseInt($("#Crust").val());
+
+                var toppingName = parseInt($("#Topping").val());
+
+                var PizzaNumber = parseInt($("#number").val());
+
+
+  var newTotal = new Pizza(flavorName, sizeName, crustName, toppingName, PizzaNumber);
+
+ console.log(newTotal)
+
+ console.log(newTotal.flavorName)
+ OrderNew.push(newTotal);
+ console.log(total)
+
+$("#Flavour").val("");
+$("#Size").val("");
+$("#Crust").val("");
+$("#Topping").val("");
+$("#number").val("");
+
+}
