f2b86766966631c817b3bf1272a6d46293e07eb3
gladyswambura <gladyswahito7@gmail.com>
2022-03-20 16:22:26 +0300

    updates the .append , to appear below the table

--- JS/script.js
+++ JS/script.js
@@ -27,17 +27,36 @@ var additionalToppings = $('#extra-toppings option:selected').val();
 var totalNumberOfPizza = $('#pizza-number').val();
 
 var totalAmount = parseInt(totalNumberOfPizza) * (parseInt(pizzaSize) + parseInt(toppings) +
-    parseInt(additionalToppings) + parseInt(crustType) + deliveryFee);
-
-//     // display summary
-//  $("#ordersTaken").append("");
-//  if (totalAmount.length > 0) {
-//      $("#form-heading").empty();
-//      $("#form-heading").append("Make A New Order");
-//  }
+    parseInt(additionalToppings) + parseInt(crustType));
 
 
+    // display summary
+// var  = new pizza(pizzaFlavor, pizzaSize, crustType, toppings, totalAmount);
 
+$("#submit").click(function () {
+    // $("#odersTaken").show();
+      $("#ordersTaken").append(
+          "<tr>" +
+          '<td>' +
+          pizzaFlavor+
+          "</td>" +
+          "<td>" +
+          pizzaSize+
+          "</td>" +
+          "<td>" +
+          crustType+
+          "</td>" +
+          "<td>" +
+          toppings+
+          "</td>" +
+          "<td>" +
+          totalAmount+
+          "</td>" +
+          "</tr>"
+
+      );
+});
+   
 // $("#totalAmount").fadeIn();
 // $("#Checkout").fadeIn();
 // $("#totalAmount").empty();
@@ -46,43 +65,36 @@ var totalAmount = parseInt(totalNumberOfPizza) * (parseInt(pizzaSize) + parseInt
 // });
 
 
-var orders = new pizza(pizzaFlavor, pizzaSize, crustType, toppings, totalAmount);
-
-$("#submit").click(function () {
-    $("#odersTaken").show();
-      $("#ordersTaken").append(
-          "<tr>" +
-          '<td scope="orders">' +
-          orders.pizzaFlavor +
-          "</td>" +
-          "<td>" +
-          orders.pizzaSize +
-          "</td>" +
-          "<td>" +
-          orders.pizzaCrust +
-          "</td>" +
-          "<td>" +
-          orders.toppings +
-          "</td>" +
-          "<td>" +
-          orders.totalAmount +
-          "</td>" +
-          "</tr>"
-
-      );
-});
-   
-// $("#checkoutForm").submit(function (event) {
-//     event.preventDefault();
-//     var name = $("#name").val();
-//     var deliveryOption = $("#OptionDelivery").val();
+// totalAmount = 0
+// for (let i = 0; i < OrderNew.length; i++) {
+//     totalAmount += OrderNew[i].totalPrice();
+//     console.log(totalAmount)
 // }
 
-// alert("Dear " + fullName + " "  + "Your" + " " + pizzaFlavor + " " + "costs ksh." + totalAmount);
 
+// $("#checkoutForm").submit(function (event) {
+//     event.preventDefault();
+//     var name = $("#person-name").val();
+//     var deliveryOption = $("#OptionDelivery").val();
+// });
 
+// $("#person-name").val("");
+// $("#OptionDelivery").val("");
+// $(".checkout-info").hide();
+// $("#Checkout").hide();
+
+// if (deliveryOption === "deliver") {
+//     $(".deliveryLocation").show();
+//     $(".deliveryCost").show();
+//     $("#totalAmount").empty();
+//     $("#delivery-cost").append(150);
+//     totalAmount += 150;
+//     $("#totalAmount").empty();
+//     $("#totalAmount").empty();
+//     $("#totalAmount").append(totalAmount);
+//     $(".TotalAmount").show();
+// } else {
+//     alert ="Dear, " + NameOfCustomer + ": Your total bill is Ksh. " + totalAmount + ".Collect your order in the next one hour." + " " + " Feel free to reach out to us anytime for questions peraining our products.");
+// // alert("Dear " + fullName + " "  + "Your" + " " + pizzaFlavor + " " + "costs ksh." + totalAmount);
 });
 });
-
-
-
