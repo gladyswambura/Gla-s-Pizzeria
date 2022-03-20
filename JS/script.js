1c0b9f3f86c5766ba73c5ff5ea91f6cf26ab76d8
gladyswambura <gladyswahito7@gmail.com>
2022-03-20 12:18:27 +0300

    creates README.md

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-var total = [];
+
 var deliveryFee = 200;
 // var crustPrice = new Array(250, 350, 300);
 // var toppingsPrice = new Array(300, 200, 150, 400);
@@ -8,93 +8,118 @@ function totalPrices(flavorName, crustName, sizeName, toppingName, pizzaNumber) 
     this.crustName = crustName;
     this.sizeName = sizeName;
     this.toppingName = toppingName;
-    this, pizzaNumber = pizzaNumber
+    this. pizzaNumber = pizzaNumber
 }
 
 $(document).ready(function () {
     $("button#submit").click(function (event) {
-        $("button#submit").alert("its you again");
          event.preventDefault();
+
+
+
+var fullName = $('#person-name').val();
+var telephoneNumber = $('#phone-number').val();
+var pizzaFlavor = $('#flavor option:selected').val();
+var pizzaSize = $('#pizza-size option:selected').val();
+var crustType = $('#crust option:selected').val();
+var toppings = $('#toppings option:selected').val();
+var additionalToppings = $('#extra-toppings option:selected').val();
+var totalNumberOfPizza = $('#pizza-number').val();
+
+let totalsOfPizza = parseInt(totalNumberOfPizza) * (parseInt(pizzaSize) + parseInt(toppings) +
+    parseInt(additionalToppings) + parseInt(crustType) + deliveryFee);
+
+    // display summary
+ $("#ordersTaken").append("");
+ if (OrderNew.length > 0) {
+     $("#form-heading").empty();
+     $("#form-heading").append("Make A New Order");
+alert("Dear " + fullName + "Your" + pizzaFlavor + " " + telephoneNumber + totalsOfPizza);
+
+
+});
 });
 
-      var pizzaPrice = new Array(500, 800, 1100);
-      Pizza.prototype.totalPrice = function () {
-          return (this.crustName + this.toppingName + this.sizeName) * this.pizzaNumber
-      };
-
-        //  size
-      Pizza.prototype.pizzaS = function () {
-          if (this.sizeName == 1100) {
-              return "Large"
-
-          } else if (this.sizeName == 800) {
-              return "Medium"
-
-          } else if (this.sizeName == 500) {
-              return "Small"
-          }
-      }
-
-    //   crust
-    Pizza.prototype.pizzaC = function () {
-        if (this.crustName == 250) {
-            return "cripsy"
-
-        } else if (this.crustName == 350) {
-            return "stuffed"
-
-        } else if (this.crustName == 300) {
-            return "Glutten-free"
-        }
-    }
-
-    // topping
-    Pizza.prototype.pizzaT = function () {
-        if (this.toppingName == 300) {
-            return "Extra-cheese"
-        } else if (this.toppingName == 200) {
-            return "Mushrooms"
-        } else if (this.toppingName == 150) {
-            return "Fresh Basil"
-        } else if (this.toppingName == 400) {
-                 return "Black olives"
-        }
-    }
-
-    $("#checkout").click(function (event) {
-
-                var flavorName = $("#Flavour").val();
-
-                var sizeName = parseInt($("#Size").val());
-
-                var crustName = parseInt($("#Crust").val());
-
-                var toppingName = parseInt($("#Topping").val());
-
-                var PizzaNumber = parseInt($("#number").val());
 
 
-  var newTotal = new Pizza(flavorName, sizeName, crustName, toppingName, PizzaNumber);
+//       var pizzaPrice = new Array(500, 800, 1100);
+//       Pizza.prototype.totalPrice = function () {
+//           return (this.crustName + this.toppingName + this.sizeName) * this.pizzaNumber
+//       };
 
- console.log(newTotal)
+//         //  size
+//       Pizza.prototype.pizzaS = function () {
+//           if (this.sizeName == 1100) {
+//               return "Large"
 
- console.log(newTotal.flavorName)
- total.push(newTotal);
- console.log(total)
+//           } else if (this.sizeName == 800) {
+//               return "Medium"
 
-$("#Flavour").val("");
-$("#Size").val("");
-$("#Crust").val("");
-$("#Topping").val("");
-$("#number").val("");
+//           } else if (this.sizeName == 500) {
+//               return "Small"
+//           }
+//       }
 
-totalAmount = 0
-for (let i = 0; i < total.length; i++) {
-    totalAmount += total[i].totalPrice();
-    console.log(totalAmount)
-}
+//     //   crust
+//     Pizza.prototype.pizzaC = function () {
+//         if (this.crustName == 250) {
+//             return "cripsy"
 
-var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' + $(".size option:selected").text() + " - " + newPizza.size + '</td><td id="toppings">' + $(".toppings option:selected").text() + " - " + newPizza.toppings + '</td><td id="crust">' + $(".crust option:selected").text() + " - " + newPizza.crust + '</td><td id="total">' + newPizza.total + '</td></tr>'
+//         } else if (this.crustName == 350) {
+//             return "stuffed"
 
-$("#pizza").append(newRow);
+//         } else if (this.crustName == 300) {
+//             return "Glutten-free"
+//         }
+//     }
+
+//     // topping
+//     Pizza.prototype.pizzaT = function () {
+//         if (this.toppingName == 300) {
+//             return "Extra-cheese"
+//         } else if (this.toppingName == 200) {
+//             return "Mushrooms"
+//         } else if (this.toppingName == 150) {
+//             return "Fresh Basil"
+//         } else if (this.toppingName == 400) {
+//                  return "Black olives"
+//         }
+//     }
+
+//     $("#checkout").click(function (event) {
+
+//                 var flavorName = $("#Flavour").val();
+
+//                 var sizeName = parseInt($("#Size").val());
+
+//                 var crustName = parseInt($("#Crust").val());
+
+//                 var toppingName = parseInt($("#Topping").val());
+
+//                 var PizzaNumber = parseInt($("#number").val());
+
+
+//   var newTotal = new Pizza(flavorName, sizeName, crustName, toppingName, PizzaNumber);
+
+//  console.log(newTotal)
+
+//  console.log(newTotal.flavorName)
+//  total.push(newTotal);
+//  console.log(total)
+
+// $("#Flavour").val("");
+// $("#Size").val("");
+// $("#Crust").val("");
+// $("#Topping").val("");
+// $("#number").val("");
+
+// totalAmount = 0
+// for (let i = 0; i < total.length; i++) {
+//     totalAmount += total[i].totalPrice();
+//     console.log(totalAmount)
+// }
+
+// var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' + $(".size option:selected").text() + " - " + newPizza.size + '</td><td id="toppings">' + $(".toppings option:selected").text() + " - " + newPizza.toppings + '</td><td id="crust">' + $(".crust option:selected").text() + " - " + newPizza.crust + '</td><td id="total">' + newPizza.total + '</td></tr>'
+
+// $("#pizza").append(newRow);
 
---
+++ README.md
@@ -0,0 +1,64 @@
+# GLA'S PIZZERIA
+
+### This is an upcoming restaurant in a comfortable, familiar, City of Nairobi that has a strong need for additional dining options. For a start the restaurant will be offering pizza toppings for anyone who orders via Our new app (Gla's Pizzeria).Enjoy!
+
+![alt text](./Images/pizzeria.jpeg)
+
+## Table of Contents
+
++ [Description](#description)
++ [Installation Requirement](#Installation)
++ [Sitelink](#Name-predictor)
++ [Technology Used](#technology-used)
++ [Reference](#reference)
++ [Licence](#licence)
++ [Authors Info](#author-Info)
+
+## Description
+<p>A pizza place that offers the yummiest pizza;s in out. You cant resist! Purchse from us.</p>
+
+
+## Installation
+<br>To install git in your pc,run (on yout terminal):<br>
+<code>sudo apt update && sudo apt upgrade</code><br>
+<code>sudo apt install git</code>
+<p>If you want to clone this repository to your local machine, use the following command:</p>
+<p><code> git clone https://github.com/gladyswambura/Gla-s-Pizzeria</code></p>
+
+## Site link
+https://github.com/gladyswambura/Gla-s-Pizzeria
+
+### Requirements
+
+* Either a computer,phone,tablet or an Ipad
+
+* An access to the Internet
+
+## Technology Used
+* HTML - which was used to build the structure of the pages.
+
+* Bootstrap - which was used to style the pages and make it responsive.
+
+* Java Script- was used to activate the buttons on the page.
+
+* Custom Css- Was used to style the pages in this website.
+
+## Reference
+* Materialize CSS
+* GetBootstrap.com
+* W3-Schools
+* jQuery.com
+
+## Licence
+
+MIT License
+
+Copyright (c) [2022] [Gladys Wambura]. All Rights Reserved.
+<a href="./LICENSE"> LICENSE</a>
+
+
+## Authors Info
+
+Slack Profile - [Gladys Wambura](https://stackoverflow.com/users/18241026/gladys-wahito?tab=profile)
+
+Linked - [Gladys Wambura](https://www.linkedin.com/in/gladys-wahito-3480a01ab/)
